cb596e563f6f90c976d336581e470288c1225cf4
Abiraman K <abiramancit@gmail.com>
2023-02-08 18:49:14 +0530

    [Abiraman K] Adds SearchResult

---
+++ src/components/SearchResult.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+interface ISearchResultProps {
+  rows: Array<any>;
+  columns: GridColDef[];
+  pageSize: number;
+  rowsPerPageOptions: Array<number>;
+  disableSelectionOnClick: boolean;
+  newEditingApi: boolean;
+  tableHeight: number;
+}
+
+export const SearchResult = (props: ISearchResultProps) => {
+  return (
+    <React.Fragment>
+      <Box sx={{ height: props?.tableHeight, width: "100%" }}>
+        <DataGrid
+          rows={props?.rows}
+          columns={props?.columns}
+          pageSize={props?.pageSize}
+          rowsPerPageOptions={props?.rowsPerPageOptions}
+          disableSelectionOnClick={props?.disableSelectionOnClick}
+          experimentalFeatures={{ newEditingApi: props?.newEditingApi }}
+        />
+      </Box>
+    </React.Fragment>
+  );
+};
