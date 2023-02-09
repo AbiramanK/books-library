a04f6cc006f4a387cc1837bc116f75a48beca7a0
Abiraman K <abiramancit@gmail.com>
2023-02-09 13:36:41 +0530

    [Abiraman K] Adds configs file

---
+++ src/configs.tsx
@@ -0,0 +1,7 @@
+export const OPEN_LIBRARY_API =
+  process.env.REACT_APP_OPEN_LIBRARY_API ?? "https://openlibrary.org";
+export const OPEN_LIBRARY_LIMIT =
+  process.env.REACT_APP_OPEN_LIBRARY_LIMIT ?? "10";
+export const OPEN_LIBRARY_FIELDS =
+  process.env.REACT_APP_OPEN_LIBRARY_FIELDS ??
+  "title,author_name,key,first_publish_year,publish_year";
