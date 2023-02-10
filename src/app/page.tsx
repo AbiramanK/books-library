9d5fc63f847c09827d1a352628eab874d6988ade
Abiraman K <abiramancit@gmail.com>
2023-02-10 11:23:08 +0530

    [Abiraman K] Refactors SearchResultDataGrid component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,132 @@
 "use client";
 import React, { useState } from "react";
 import { Grid } from "@mui/material";
+import { useSnackbar } from "notistack";
+import axios from "axios";
+
 import { SearchResultDataGrid, TopPanel } from "@/components";
+import {
+  OPEN_LIBRARY_API,
+  OPEN_LIBRARY_FIELDS,
+  OPEN_LIBRARY_LIMIT,
+} from "@/configs";
+import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  {
+    field: "title",
+    headerName: "Title and Sub Title",
+    width: 250,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params?.row?.title ?? "-"}`,
+  },
+  {
+    field: "author_name",
+    headerName: "Author",
+    width: 150,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params?.row?.author_name?.[0] ?? "-"}`,
+  },
+  {
+    field: "publish_year",
+    headerName: "Last Publish Year",
+    width: 150,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params?.row?.publish_year !== 0 ? params?.row?.publish_year : "-"}`,
+  },
+  {
+    field: "first_publish_year",
+    headerName: "First Publish Year",
+    width: 150,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params?.row?.first_publish_year ?? "-"}`,
+  },
+];
+
+export interface SearchResultDocsInterface {
+  title: string;
+  publish_year: Array<number>;
+  first_publish_year: number;
+  author_name: Array<string>;
+}
+
+export interface SearchResultDocsAPIInterface
+  extends SearchResultDocsInterface {
+  key: string;
+}
+export interface SearchResultDocsTableInterface
+  extends Omit<SearchResultDocsInterface, "publish_year"> {
+  id: string;
+  publish_year: number;
+}
+export interface SearchResultInterface {
+  numFound: number;
+  start: number;
+  numFoundExact: boolean;
+  docs: SearchResultDocsAPIInterface[];
+  num_found: number;
+  q: string;
+  offset: any;
+}
 
 export default function Home() {
-  const [searchText, setSearchText] = useState<string>("");
+  const { enqueueSnackbar } = useSnackbar();
 
-  const searchBooks = async (search: string) => {
+  const [searchText, setSearchText] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<SearchResultInterface>();
+  const [searchResultDocs, setSearchResultDocs] =
+    useState<SearchResultDocsTableInterface[]>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [lastPage, setLastPage] = useState<boolean>(false);
+
+  const searchBooks = async (search: string, currentPage?: number) => {
     setSearchText(search);
+
+    try {
+      if (!currentPage && page != 1) {
+        setPage(1);
+        currentPage = 1;
+      }
+
+      setLoading(true);
+      const {
+        data,
+        status,
+        statusText,
+      }: { data: SearchResultInterface; status: number; statusText: string } =
+        await axios.get(`${OPEN_LIBRARY_API}/search.json`, {
+          params: {
+            q: search,
+            limit: OPEN_LIBRARY_LIMIT,
+            page: currentPage ?? page,
+            fields: OPEN_LIBRARY_FIELDS,
+          },
+        });
+
+      setLoading(false);
+      if (status === 200) {
+        setSearchResult(data);
+        const docs: SearchResultDocsTableInterface[] = data?.docs?.map(
+          ({ key: id, publish_year, ...rest }) => {
+            const lastPublishYear = Math?.max(...(publish_year ?? [0]));
+            return {
+              id,
+              publish_year: lastPublishYear,
+              ...rest,
+            };
+          }
+        );
+
+        setSearchResultDocs(docs);
+      } else {
+        enqueueSnackbar("Something went wrong", { variant: "error" });
+      }
+    } catch (error: any) {
+      setLoading(false);
+      console.error("Search Books requeest: ", error?.message);
+      enqueueSnackbar(error?.message, { variant: "error" });
+    }
   };
 
   return (
@@ -17,7 +136,17 @@ export default function Home() {
           <TopPanel searchBooks={searchBooks} />
         </Grid>
         <Grid item xs={12}>
-          <SearchResultDataGrid search={searchText} />
+          <SearchResultDataGrid
+            searchKey={searchText}
+            search={searchBooks}
+            searchResultData={searchResultDocs}
+            loading={loading}
+            startPage={searchResult?.start!}
+            endPage={searchResult?.start! + searchResult?.docs?.length!}
+            totalPage={searchResult?.numFound!}
+            columns={columns}
+            searchResultLimit={parseInt(OPEN_LIBRARY_LIMIT!)}
+          />
         </Grid>
       </Grid>
     </React.Fragment>
--- src/components/SearchResultDataGrid.tsx
+++ src/components/SearchResultDataGrid.tsx
@@ -1,158 +1,44 @@
-import {
-  OPEN_LIBRARY_API,
-  OPEN_LIBRARY_FIELDS,
-  OPEN_LIBRARY_LIMIT,
-} from "@/configs";
-import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-import axios from "axios";
-import { useSnackbar } from "notistack";
+import { GridColDef } from "@mui/x-data-grid";
 import React, { useEffect, useState } from "react";
 import { PaginationComponent } from "./PaginationComponent";
 import { SearchResult } from "./SearchResult";
 
-const columns: GridColDef[] = [
-  {
-    field: "title",
-    headerName: "Title and Sub Title",
-    width: 250,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params?.row?.title ?? "-"}`,
-  },
-  {
-    field: "author_name",
-    headerName: "Author",
-    width: 150,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params?.row?.author_name?.[0] ?? "-"}`,
-  },
-  {
-    field: "publish_year",
-    headerName: "Last Publish Year",
-    width: 150,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params?.row?.publish_year !== 0 ? params?.row?.publish_year : "-"}`,
-  },
-  {
-    field: "first_publish_year",
-    headerName: "First Publish Year",
-    width: 150,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params?.row?.first_publish_year ?? "-"}`,
-  },
-];
-
-export interface SearchResultDocsInterface {
-  title: string;
-  publish_year: Array<number>;
-  first_publish_year: number;
-  author_name: Array<string>;
-}
-
-export interface SearchResultDocsAPIInterface
-  extends SearchResultDocsInterface {
-  key: string;
-}
-export interface SearchResultDocsTableInterface
-  extends Omit<SearchResultDocsInterface, "publish_year"> {
-  id: string;
-  publish_year: number;
-}
-export interface SearchResultInterface {
-  numFound: number;
-  start: number;
-  numFoundExact: boolean;
-  docs: SearchResultDocsAPIInterface[];
-  num_found: number;
-  q: string;
-  offset: any;
-}
-
 interface ISearchResultDataGridProps {
-  search?: string;
+  searchKey: string;
+  search: (search: string, currentPage?: number) => Promise<void>;
+  searchResultData: any[] | undefined;
+  loading: boolean;
+  startPage: number;
+  endPage: number;
+  totalPage: number;
+  columns: GridColDef[];
+  searchResultLimit: number;
 }
 
 export const SearchResultDataGrid = (props: ISearchResultDataGridProps) => {
-  const { enqueueSnackbar } = useSnackbar();
-
-  const [searchResult, setSearchResult] = useState<SearchResultInterface>();
-  const [searchResultDocs, setSearchResultDocs] =
-    useState<SearchResultDocsTableInterface[]>();
-  const [loading, setLoading] = useState<boolean>(false);
   const [page, setPage] = useState<number>(1);
-  const [searchText, setSearchText] = useState<string>("");
+
   const [lastPage, setLastPage] = useState<boolean>(false);
 
   useEffect(() => {
-    searchBooks(props?.search!);
-  }, [props?.search]);
-
-  useEffect(() => {
     const totalPages =
-      (page - 1) * parseInt(OPEN_LIBRARY_LIMIT!) + searchResultDocs?.length!;
-    if (totalPages === searchResult?.numFound!) {
+      (page - 1) * props?.searchResultLimit + props?.searchResultData?.length!;
+    if (totalPages === props?.totalPage!) {
       setLastPage(true);
     } else {
       if (lastPage) {
         setLastPage(false);
       }
     }
-  }, [searchResultDocs]);
-
-  const searchBooks = async (search: string, currentPage?: number) => {
-    setSearchText(search);
-    try {
-      if (!currentPage && page != 1) {
-        setPage(1);
-        currentPage = 1;
-      }
-
-      setLoading(true);
-      const {
-        data,
-        status,
-        statusText,
-      }: { data: SearchResultInterface; status: number; statusText: string } =
-        await axios.get(`${OPEN_LIBRARY_API}/search.json`, {
-          params: {
-            q: search,
-            limit: OPEN_LIBRARY_LIMIT,
-            page: currentPage ?? page,
-            fields: OPEN_LIBRARY_FIELDS,
-          },
-        });
-
-      setLoading(false);
-      if (status === 200) {
-        setSearchResult(data);
-        const docs: SearchResultDocsTableInterface[] = data?.docs?.map(
-          ({ key: id, publish_year, ...rest }) => {
-            const lastPublishYear = Math?.max(...(publish_year ?? [0]));
-            return {
-              id,
-              publish_year: lastPublishYear,
-              ...rest,
-            };
-          }
-        );
-
-        setSearchResultDocs(docs);
-      } else {
-        enqueueSnackbar("Something went wrong", { variant: "error" });
-      }
-    } catch (error: any) {
-      setLoading(false);
-      console.error("Search Books requeest: ", error?.message);
-      enqueueSnackbar(error?.message, { variant: "error" });
-    }
-  };
+  }, [props?.searchResultData]);
 
   const nextPage = () => {
-    const total: number = searchResult?.numFound!;
+    const total: number = props?.totalPage!;
 
-    if (page * parseInt(OPEN_LIBRARY_LIMIT!) < total) {
+    if (page * props?.searchResultLimit < total) {
       const nextPage = page + 1;
       setPage(nextPage);
-      searchBooks(searchText, nextPage);
+      props?.search(props?.searchKey ?? "", nextPage);
     }
   };
 
@@ -160,29 +46,29 @@ export const SearchResultDataGrid = (props: ISearchResultDataGridProps) => {
     if (page > 1) {
       const previousPage: number = page - 1;
       setPage(previousPage);
-      searchBooks(searchText, previousPage);
+      props?.search(props?.searchKey ?? "", previousPage);
     }
   };
 
   return (
     <React.Fragment>
       <SearchResult
-        columns={columns}
-        rows={searchResultDocs ?? []}
+        columns={props?.columns}
+        rows={props?.searchResultData ?? []}
         disableSelectionOnClick={true}
         newEditingApi={true}
         pageSize={10}
         rowsPerPageOptions={[10]}
         tableHeight={380}
-        loading={loading}
+        loading={props?.loading}
       />
       <PaginationComponent
-        startPage={searchResult?.start!}
-        endPage={searchResult?.start! + searchResult?.docs?.length!}
-        totalPage={searchResult?.numFound!}
+        startPage={props?.startPage}
+        endPage={props?.endPage}
+        totalPage={props?.totalPage}
         nextPage={nextPage}
         previousPage={previousPage}
-        nextButtonDisabled={lastPage || !searchResult}
+        nextButtonDisabled={lastPage || !props?.searchResultData}
         previousButtonDisabled={page == 1}
       />
     </React.Fragment>
