7edf1469baa19da3f1c0bcd4b2a592c90805cdf7
Abiraman K <abiramancit@gmail.com>
2023-02-24 19:57:12 +0530

    [Abiraman K] Adds snapshot  for ListSkeleton component

--- __test__/snapshots/index.test.tsx
+++ __test__/snapshots/index.test.tsx
@@ -1,11 +1,22 @@
 import TestRenderer from "react-test-renderer";
 
 import Head from "@/app/head";
+import { ListSkeleton } from "@/components";
 
 describe("Snapshots", () => {
-  it("App <Head /> should render title and description", () => {
-    const tree = TestRenderer.create(<Head />).toJSON();
+  describe("Layout => App => <Head />", () => {
+    it("should render title and description", () => {
+      const tree = TestRenderer.create(<Head />).toJSON();
 
-    expect(tree).toMatchSnapshot();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe("Component => <ListSkeliton />", () => {
+    it("should render 2 skeletons", () => {
+      const tree = TestRenderer.create(<ListSkeleton length={2} />).toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
   });
 });
